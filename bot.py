d371efc94d6cd43b4893a2b5b7fdb7430445b520
ziracook <ziracook@gmail.com>
2019-04-02 22:43:09 -0700

    bot now filters posts and adds them to respective dictionaries

--- bot.py
+++ bot.py
@@ -1,38 +1,54 @@
 #!/usr/bin/python
 
 import praw
+import csv
+import pprint
 
-GROUP_BUY = ["GROUP BUY", "GB", "GROUPBUY"]
-INTEREST_CHECK = ["IC", "INTEREST CHECK", "INTERESTCHECK"]
+GROUP_BUY = ["[GB]", "GROUP BUY", "GROUPBUY"]
+INTEREST_CHECK = ["[IC]", "INTEREST CHECK", "INTERESTCHECK"]
+groupBuys = dict()
+interestChecks = dict()
 
 def isGroupBuyPost(postTitle):
     postTitle = postTitle.upper()
-    isGroupBuy = false
+    isGroupBuy = False
     for s in GROUP_BUY:
         if s in postTitle:
-            isGroupBuy = true
+            isGroupBuy = True
     return isGroupBuy
 
 def isInterestCheckPost(postTitle):
     postTitle = postTitle.upper()
-    isInterestCheck = false
+    isInterestCheck = False
     for s in INTEREST_CHECK:
         if s in postTitle:
-            isInterestCheck = true
+            isInterestCheck = True
     return isInterestCheck 
 
-def checkRecentPosts():
-
+def appendDiciontaryToCsv(fileName, dictionary):
+    with open(fileName, 'a', newline='') as csvfile:
+        fieldnames = ['postTitle', 'postURL']
+        csvwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        csvwriter.writeheader()    
+        
+        for title, url in dictionary.items():
+            csvwriter.writerow({'postTitle': title, 'postURL': url})
 
 def main():
     reddit = praw.Reddit('bot')
     subreddit = reddit.subreddit("mechmarket")
+    postsChecked = 0
 
-    groupBuyFile = open('group_buys.csv', 'a')
-    interestCheckFile = open('interest_checks.csv', 'a')
-
-    for submission in subreddit.hot(limit=5):
-        print("Title: ", submission.title)
-        print("URL: ", submission.url)
+    for post in subreddit.hot(limit=20):
+        postsChecked += 1  
+        if isGroupBuyPost(post.title):
+            groupBuys[post.title] = post.url
+        elif isInterestCheckPost(post.title):
+            interestChecks[post.title] = post.url 
+    print(postsChecked)
 
 main()
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(groupBuys)
+pp.pprint(interestChecks)
+
