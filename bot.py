17afccb99de5de141b04a411163ab4c61d6b1036
ziracook <ziracook@gmail.com>
2019-04-02 21:55:45 -0700

    added checks for group buy and interest checks

--- bot.py
+++ bot.py
@@ -2,10 +2,37 @@
 
 import praw
 
-reddit = praw.Reddit('bot')
+GROUP_BUY = ["GROUP BUY", "GB", "GROUPBUY"]
+INTEREST_CHECK = ["IC", "INTEREST CHECK", "INTERESTCHECK"]
 
-subreddit = reddit.subreddit("mechmarket")
+def isGroupBuyPost(postTitle):
+    postTitle = postTitle.upper()
+    isGroupBuy = false
+    for s in GROUP_BUY:
+        if s in postTitle:
+            isGroupBuy = true
+    return isGroupBuy
 
-for submission in subreddit.hot(limit=5):
-    print("Title: ", submission.title)
-    print("URL: ", submission.url)
+def isInterestCheckPost(postTitle):
+    postTitle = postTitle.upper()
+    isInterestCheck = false
+    for s in INTEREST_CHECK:
+        if s in postTitle:
+            isInterestCheck = true
+    return isInterestCheck 
+
+def checkRecentPosts():
+
+
+def main():
+    reddit = praw.Reddit('bot')
+    subreddit = reddit.subreddit("mechmarket")
+
+    groupBuyFile = open('group_buys.csv', 'a')
+    interestCheckFile = open('interest_checks.csv', 'a')
+
+    for submission in subreddit.hot(limit=5):
+        print("Title: ", submission.title)
+        print("URL: ", submission.url)
+
+main()
